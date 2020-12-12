bba65c957a85661a224d6b096606d6ebeec87f7a
Oscar1011 <shengbinxin@gmail.com>
2020-12-12 21:29:47 +0800

    Update embyPremiere.js

--- rule/embyPremiere.js
+++ rule/embyPremiere.js
@@ -1,4 +1,4 @@
-if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
+  if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
       if($response.status!=200){
           $done({status: 200, headers: $response.headers, body: '{"cacheExpirationDays":999,"resultCode":"GOOD","message":"Device Valid"}' })
       }else{
